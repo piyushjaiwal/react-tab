f2983a320c9ee3755bc7e4dc88af445b7ed3baf3
piyushjaiwal <>
2020-08-22 19:25:13 +0530

    Updates

---
+++ precache-manifest.617e5f67927cde43e344deb1c3247e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa05620850a1b328b0aabf8da689cbb",
+    "url": "/react-tab/index.html"
+  },
+  {
+    "revision": "1252e06a2e6fc27e95cb",
+    "url": "/react-tab/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "211f98dfdbdae1d3f6e5",
+    "url": "/react-tab/static/js/2.00e209ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tab/static/js/2.00e209ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1252e06a2e6fc27e95cb",
+    "url": "/react-tab/static/js/main.e0e15d39.chunk.js"
+  },
+  {
+    "revision": "9e088ad7afa322c17023",
+    "url": "/react-tab/static/js/runtime-main.2f8086f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tab/static/media/logo.5d5d9eef.svg"
+  }
+]);
